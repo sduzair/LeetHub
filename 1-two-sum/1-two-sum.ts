d69c8ceab9f292fe778f02498f53304e93432a71
Uzair <52709741+sduzair@users.noreply.github.com>
2022-09-14 11:17:38 -0400

    Time: 264 ms (15.38%), Space: 44.4 MB (87.27%) - LeetHub

---
+++ 1-two-sum/1-two-sum.ts
@@ -0,0 +1,10 @@
+function twoSum(nums: number[], target: number): number[] {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            if ( i === j ) continue;
+            if (nums[i] + nums[j] === target) {
+                return [i, j]
+            }
+        }
+    }
+};
